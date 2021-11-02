73d2cff62a6b80d4f0255f85cfd7ef4d5dd06484
Bhushan Rajam <rajambhushan123@gmail.com>
2021-11-02 10:46:49 +0530

    Test Demo Committed by Bhushan Rajam

---
+++ src/main/java/Bhushan1.java
@@ -0,0 +1,6 @@
+public class Bhushan1{
+    public static int add(int number1,int number2)
+    {
+        return number1+number2;
+    }
+}
---
+++ src/test/java/Bhushan1Test.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+
+public class Bhushan1Test {
+
+    @org.junit.Test
+    public void add() {
+        int result= Bhushan1.add(3,3);
+        int correct= 6;
+        assertEquals(6,result);
+    }
+}
